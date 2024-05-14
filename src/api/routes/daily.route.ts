19bbd0ee83243a36d8fc021dcf6e7e00191eb370
commerce01 <viranchana.a62@rsu.ac.th>
2024-05-14 14:18:10 +0700

    update

--- src/api/routes/daily.route.ts
+++ src/api/routes/daily.route.ts
@@ -31,26 +31,6 @@ function findAvgByHour(
 }
 
 const router = Router();
-router.post("/daily-level", async (req, res) => {
-  const { pm25Level, co2Level } = req.body;
-
-  const avgPm25 =
-    pm25Level.reduce((acc: number, cur: number) => acc + cur, 0) /
-    pm25Level.length;
-  const avgCo2 =
-    co2Level.reduce((acc: number, cur: number) => acc + cur, 0) /
-    co2Level.length;
-
-  const dailydust = await db.dailyDustLevel.create({
-    data: {
-      pm25Level,
-      co2Level,
-      avgpm25Level: avgPm25,
-      avgco2Level: avgCo2,
-    },
-  });
-  return res.json(dailydust);
-});
 
 router.get("/daily-level", async (req, res) => {
   const { date } = req.query;
